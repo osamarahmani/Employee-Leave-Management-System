de2e9c0b48619ac5c2bf2cfa004f1abbf1a9691f
ElizJuditSharp <elizjuditsharp@gmail.com>
2026-06-03 15:15:13 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ def get_db():
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="root",    # ← change this to YOUR MySQL password
-        database="leave_db1"
+        password="root",  
+        database="leave_db"
     )
     return conn
